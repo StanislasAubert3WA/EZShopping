2f12d68d70a12295fb2d058762d48a67b7de76d9
oliviet <oliviet92@gmail.com>
2023-02-15 21:44:01 +0700

    avec la nav dans la Userpage et le login en moins

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UserForm from "../components/UserForm";
+import Nav from "../components/Nav";
 
 function User(){
     return(
         <div>
+            <Nav />
             <UserForm />
         </div>
     )
