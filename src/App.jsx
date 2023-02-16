2528969ceef29ee27f285b5a608f2329cba3be69
oliviet <oliviet92@gmail.com>
2023-02-16 17:53:08 +0700

    le localstorage est plus sauvegarde 20 fois au debut

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import User from './pages/UserPage';
+import CartPage from './pages/CartPage';
 import './App.css';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
           {/* <Route path="/" element={<Nav />}> */}
             <Route index element={<Home />} />
             <Route path="user" element={<User />} />
-            {/*<Route path="user" element={<UserPage />} />*/}
+            <Route path="user/cart" element={<CartPage />} />
             {/*<Route path="*" element={<NoPage />} />*/}
           {/*</Route>*/}
         </Routes>
